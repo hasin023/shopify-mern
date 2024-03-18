4888de5067655ff74253b3194e5bf45cf08960b1
Hasin <hasinmahtab.alvee@gmail.com>
2024-03-18 19:40:32 +0600

    Added product model, controller, routes

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
-// import productRoutes from "./routes/productRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 // import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
-// app.use("/api/products", productRoutes);
+app.use("/api/products", productRoutes);
 // app.use("/api/upload", uploadRoutes);
 
 // const __dirname = path.resolve();
